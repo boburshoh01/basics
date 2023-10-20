f31d303f5e19caa0664381a689c1505b74dcc96c
boburshoh01 <temirovbobur8@gmail.com>
2023-10-20 21:08:18 +0500

    news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
 
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'Luis';
+  imgURL='https://picsum.photos/id/237/500/500'
+
+  getName(){
+    return this.name
+  }
+
+  changeImage(e:KeyboardEvent){
+    this.imgURL=(e.target as HTMLInputElement).value
+  }
 }
